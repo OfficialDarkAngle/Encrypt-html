8e83836e9317e50de0e19d1b50d50de6869f325f
OfficialDarkAngle <67592435+OfficialDarkAngle@users.noreply.github.com>
2020-07-23 21:11:37 +0700

    Create bener.py

---
+++ bener.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+
+import os,sys,time,random
+
+os.system('clear')
+
+def mengetik(s):
+
+    for c in s + '\n':
+
+        sys.stdout.write(c)
+
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+mengetik('  ▼￣＞-―-＜￣▼       ~  ~  ~   ┌∩┐(◣_◢)┌∩┐   ~  ~  ~\n   Ｙ        Ｙ    ╔╦╗╔═╗╦═╗╔╦╗╦ ╦═╗ ╦\n/\ /   ●  ω ●）    ║ ╠╣ ╠╦╝║║║║ ║╔╩╦╝ <•>\n\ ｜　  つ　  ヽつ  ╩ ╚═╝╩╚═╩ ╩╚═╝╩ ╚═\n[ encrypt html ]  [ Anti Recode Source Code Website   ]\n╔══════════════════════════════════════════════════════════╗\n║  {•}  Author    :   Noiz ID                              ║\n║  {•}  Team      :   - PARANOID CYBER  ️                   ║\n║                     -【ᏒᎶ4】ʙʟᴀᴄᴋ  ◤coᴅᴇʀ◢ ツ            ║\n║                     - Black Coder Crush                  ║\n║  {•}  Github    :   https://github.com/OfficialDarkAngle ║\n╚══════════════════════════════════════════════════════════╝\n\n              SAVE GAZA PALESTINA\n')
